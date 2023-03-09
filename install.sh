f66532d182563b59228505259717e26e6df872ee
iQuick <iquickpublic@gmail.com>
2023-03-09 10:50:36 +0100

    added packages

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-paru -S fastfetch fish i3-gaps kitty nitrogen polybar rofi picom-pijulius-git pfetch cava &&
+paru -S fastfetch fish i3-gaps kitty nitrogen polybar rofi picom-pijulius-git pfetch cava ttf-font-awesome ttf-iosevka doas bat exa &&
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish &&
 mv .config $HOME
