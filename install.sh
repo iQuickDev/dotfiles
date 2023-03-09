d2072b72910fd1fbe4aed8ffe657bc1209b0e1b7
iQuick <iquickpublic@gmail.com>
2023-03-09 11:07:38 +0100

    added ohmyfish

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 paru -S fastfetch fish i3-gaps kitty nitrogen polybar rofi picom-pijulius-git pfetch cava ttf-font-awesome ttf-iosevka doas bat exa &&
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish &&
-mv .config $HOME
+omf install agnoster
+mv .config/* $HOME/.config
