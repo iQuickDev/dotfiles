db56c4470191e9638211103b82c00a8e2d766de0
iQuick <iquickpublic@gmail.com>
2023-05-14 01:10:00 +0200

    fixed icons for rofi + updated README

---
+++ README.md
@@ -0,0 +1,9 @@
+# My Dotfiles
+[⚠️] The install script may not work
+
+## Screenshots
+![](screenshots/desktop.png)
+![](screenshots/apps.png)
+![](screenshots/code.png)
+
+My setup is heavily relies on the [Catppuccin](https://github.com/catppuccin) project
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-paru -S fastfetch fish i3-gaps kitty nitrogen polybar rofi picom-pijulius-git pfetch cava ttf-font-awesome ttf-iosevka doas bat exa &&
+paru -S fastfetch fish i3-gaps kitty nitrogen polybar rofi picom-pijulius-git pfetch cava ttf-font-awesome ttf-hack doas bat exa &&
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish &&
 omf install agnoster &&
-chsh -s /usr/bin/fish
+chsh -s /usr/bin/fish &&
 mv .config/* $HOME/.config
